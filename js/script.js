5c99f72006c8ba6ae5b6c9c4aef3d58f8709219a
xiaoshuolim <86652686+xiaoshuolim@users.noreply.github.com>
2025-03-03 01:30:36 +0800

    script.js changes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+// Upon page load
+document.addEventListener("DOMContentLoaded", function () {
+    document.body.classList.add("loaded");
+    document.querySelector(".landingtext").classList.add("loaded");
+});
+
+// Testimonials
 let slidePosition = 0;
 const slides = document.getElementsByClassName('item');
 const totalSlides = slides.length;
@@ -38,3 +45,17 @@ function moveToPrevSlide(){
 }
 
 updateSlidePosition();
+
+// Hamburger menu
+const menuContainer = document.querySelector(".menu-container");
+const menuItemsContainer = document.querySelector(".menu-items-container");
+
+menuContainer.addEventListener("click", () => {
+  menuContainer.classList.toggle("active");
+
+  if (menuContainer.classList.contains("active")) {
+    menuItemsContainer.classList.add("active");
+  } else {
+    menuItemsContainer.classList.remove("active");
+  }
+});
